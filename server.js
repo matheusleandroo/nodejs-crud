559e8956a56fc21b1973f82b03f1f7d9f7decf2a
Matheus Leandro <mattleandroo@outlook.com>
2019-09-29 18:54:10 -0300

    add connect param

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/CRUD', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 requireDir('./src/models');
